c750026da408914cd53b726c8cda58603d9d2410
EXP <289065406@qq.com>
2020-11-15 02:19:22 +0800

    init

---
+++ run.ps1
@@ -0,0 +1,21 @@
+$DOCKER_ID = (docker ps -aq --filter name=docker_kali)
+if([String]::IsNullOrEmpty(${DOCKER_ID})) {
+
+    echo "The kali is building ..."
+    docker-compose up -d
+
+    echo "Waiting for postgres db up ..."
+    Sleep 30
+
+    echo "All is up"
+}
+
+
+if(![String]::IsNullOrEmpty(${DOCKER_ID})) {
+
+    echo "Init msf db ..."
+    docker exec ${DOCKER_ID} /bin/bash -c "/usr/bin/msfdb init"
+
+    echo "Openning the kali terminal ..."
+    docker exec -it ${DOCKER_ID} /bin/bash
+}
---
+++ stop.ps1
@@ -0,0 +1,2 @@
+docker-compose down
+echo "Kali is down"
